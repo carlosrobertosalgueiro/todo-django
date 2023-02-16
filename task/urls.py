9677d3dc76f9988b0a68c51b76c9c10d5d0d501e
carlos roberto <carlosrobertounopar01@hotmail.com>
2023-02-16 13:56:04 -0300

    aprendendo views

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+#importa o views
 from . import views
 
 urlpatterns = [
+    #Url de rota estática
     path('helloword/', views.helloword ),
+    #Url de rota estatica sendo a rota principal
+    path('', views.tasklist, name='tasklist'),
+    #recebe uma valor via url
+    path('stringname/<str:name>', views.yourname, name='yourname')
 ]
